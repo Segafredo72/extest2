8c392e0c905c3ad2935237bcaf17164a78d20718
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-10-02 11:37:52 +0200

    ExA3

--- ExA1/src/test/java/be/abis/exercise/ut/TestPerson.java
+++ ExA1/src/test/java/be/abis/exercise/ut/TestPerson.java
@@ -1,11 +1,23 @@
 package be.abis.exercise.ut;
 
+import be.abis.exercise.model.Person;
 import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class TestPerson {
 
     @Test
     public void personShouldBe42(){
+        //arrange
+        int expected = 42;
+        Person p = new Person(1, "John","Doe", LocalDate.of(1978, 8, 10));
+        //act
+        int age = p.calculateAge()+1;
+        // assert
+        assertEquals(expected, age);
 
     }
 }
