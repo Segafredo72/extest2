4e8f153e46ae767557e38a8b1d0a019695bf0a92
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-10-04 14:45:06 +0200

    Add ExA6

---
+++ ExA6/src/test/java/TestAddress.java
@@ -0,0 +1,18 @@
+import be.abis.exercise.model.Address;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestAddress {
+
+    @Test
+    public void belgianZipCodeShouldBeNumeric(){
+
+        //Aarange
+        Address a = new Address("Diestesteenweg","32 bus 4B","3000","Leuven","Belgie","BE");
+        //Act
+        boolean b = a.checkBelgianZipCode();
+        //Assess
+        assertTrue(b);
+    }
+}
