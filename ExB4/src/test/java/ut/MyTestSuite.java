2fd616240d1309e76d51e4f00ac2125c490def28
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-10-05 12:17:37 +0200

    Add ExB4

---
+++ ExB4/src/test/java/ut/MyTestSuite.java
@@ -0,0 +1,9 @@
+package ut;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { TestPerson.class, TestAddress.class} )
+public class MyTestSuite {
+}
---
+++ ExB4/src/test/java/ut/TestPerson.java
@@ -0,0 +1,43 @@
+package ut;
+
+import exercise.exception.PersonShouldBeAdultException;
+import exercise.model.Person;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+
+public class TestPerson {
+
+    private Person p;
+
+    @Before
+    public void setUp(){
+        p = new Person(1, "John","Doe", LocalDate.of(1978, 8, 10));
+    }
+
+    @Test
+    public void personShouldBe42() throws PersonShouldBeAdultException {
+         // assert
+        assertThat(p.calculateAge(), is (equalTo(42)));
+
+    }
+
+    @Test
+    public void toStringSentenceStartsWithPerson(){
+         // assert
+        assertThat(p.toString(), startsWith("Person"));
+
+    }
+
+    @Test(expected= PersonShouldBeAdultException.class)
+    public void calculateAgeShouldThrowExceptionWhenPersonNotAdult() throws PersonShouldBeAdultException {
+        Person p2 = new Person(2,"Jane","Smith",LocalDate.of(2007, 8, 10));
+        p2.calculateAge();
+    }
+}
